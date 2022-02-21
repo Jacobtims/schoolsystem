a625a681fe4756c3267856eba34eea2e76d95d51
Jacobtims <jacobtimmer04@gmail.com>
2022-02-21 18:06:51 +0100

    Removed useless jetstream files
    Moved important files to 'resources/OLD' map

--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -5,7 +5,6 @@
         <div class="pt-4 bg-gray-100">
             <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
                 <div>
-                    <jet-authentication-card-logo />
                 </div>
 
                 <div v-html="policy" class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
@@ -18,14 +17,12 @@
 <script>
 import { defineComponent } from 'vue'
 import { Head } from '@inertiajs/inertia-vue3';
-import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
 
 export default defineComponent({
     props: ['policy'],
 
     components: {
         Head,
-        JetAuthenticationCardLogo,
     },
 })
 </script>
