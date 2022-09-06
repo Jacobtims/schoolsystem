4e698f9ac947cb92848e7b2723837a6ee4b5981f
Jacob2 <jacobtimmer04@gmail.com>
2022-09-06 17:20:05 +0200

    Add 'FormModel', 'Input' and 'Checkbox' component

---
+++ resources/js/components/Inputs/Input.vue
@@ -0,0 +1,31 @@
+<template>
+    <label :for="id" class="form-label">{{ label }}</label>
+    <input class="form-control" :class="{'is-invalid': error}" :aria-describedby="feedback" :id="id"
+           :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" v-bind="$attrs">
+    <div :id="feedback" class="invalid-feedback" v-if="error">
+        {{ error }}
+    </div>
+    <slot/>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    props: {
+        modelValue: {
+            default: "",
+            type: String
+        },
+        label: String,
+        error: String
+    },
+    computed: {
+        id() {
+            return "input" + this.label;
+        },
+        feedback() {
+            return "feedback" + (this.id[0].toUpperCase() + this.id.slice(1));
+        }
+    }
+}
+</script>
