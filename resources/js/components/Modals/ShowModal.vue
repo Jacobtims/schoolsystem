0164da0df7a57c81da837b8d43c8e2169eee9354
Jacob <jacobtimmer04@gmail.com>
2022-09-06 21:24:35 +0200

    Add 'ShowModal' component

---
+++ resources/js/components/Modals/ShowModal.vue
@@ -0,0 +1,22 @@
+<template>
+    <Dialog v-model:visible="open" :breakpoints="{'1200px': '50vw', '992px': '65vw'}" :style="{width: '40vw'}"
+        :header="header" :draggable="false" :modal="true" @hide="$emit('close')">
+        <div>
+            <slot/>
+        </div>
+    </Dialog>
+</template>
+
+<script>
+import Dialog from "primevue/dialog";
+
+export default {
+    components: {
+        Dialog
+    },
+    props: {
+        open: Boolean,
+        header: String
+    }
+}
+</script>
