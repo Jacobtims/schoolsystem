38b7d1126e075bde22f941043ebabf73147194f2
Jacob <jacobtimmer04@gmail.com>
2022-10-26 18:21:37 +0200

    Fix issue that local folder has no uppercase and on github it does
    resources/js/components -> resources/js/Components

---
+++ resources/js/Components/Inputs/Checkbox.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="form-check">
+        <input class="form-check-input" :class="{'is-invalid': error}" type="checkbox" :aria-describedby="feedback" :id="id"
+               :checked="modelValue" @input="$emit('update:modelValue', $event.target.checked)" v-bind="$attrs">
+        <label class="form-check-label" :for="id">{{ label }}</label>
+    </div>
+    <div :id="feedback" class="invalid-feedback" v-if="error">
+        {{ error }}
+    </div>
+    <slot/>
+</template>
+<script>
+export default {
+    inheritAttrs: false,
+    props: {
+        modelValue: {
+            default: null,
+            type: Boolean
+        },
+        label: String,
+        error: String
+    },
+    computed: {
+        id() {
+            return "checkbox" + this.label;
+        },
+        feedback() {
+            return "feedback" + (this.id[0].toUpperCase() + this.id.slice(1));
+        }
+    }
+}
+</script>
---
+++ resources/js/Components/Modals/FormModal.vue
@@ -0,0 +1,35 @@
+<template>
+    <Dialog v-model:visible="open" :breakpoints="{'1200px': '50vw', '992px': '65vw'}" :style="{width: '40vw'}"
+        :header="header" :draggable="false" :modal="true" @hide="$emit('close')">
+        <div>
+            <slot/>
+        </div>
+
+        <template #footer>
+            <button class="btn btn-primary" type="submit" @click="$emit('action')" autofocus :disabled="disabled">
+                <i class="fa-solid fa-check"></i> {{ submitText }}</button>
+            <button class="btn btn-secondary" @click="$emit('close')" :disabled="disabled">
+                <i class="fa-solid fa-xmark"></i> {{ cancelText }}</button>
+        </template>
+    </Dialog>
+</template>
+
+<script>
+import Dialog from "primevue/dialog";
+
+export default {
+    components: {
+        Dialog
+    },
+    props: {
+        open: Boolean,
+        header: String,
+        disabled: Boolean,
+        submitText: String,
+        cancelText: {
+            default: 'Annuleren',
+            type: String,
+        }
+    }
+}
+</script>
