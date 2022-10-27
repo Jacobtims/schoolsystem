c90f262753f885563daf938727d96a424b81c8f0
Jacob <jacobtimmer04@gmail.com>
2022-10-27 19:47:27 +0200

    Fix scrolling behavior in 'main' section

--- resources/js/Layouts/StudentLayout.vue
+++ resources/js/Layouts/StudentLayout.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex w-full min-h-screen bg-gray-100">
         <Sidebar/>
-        <div class="w-full">
+        <div class="w-full flex flex-col h-screen">
             <Navbar/>
 
-            <main class="m-10">
+            <main class="p-10 flex-1 overflow-auto">
                 <slot/>
             </main>
         </div>
